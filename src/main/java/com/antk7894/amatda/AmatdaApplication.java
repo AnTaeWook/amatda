5d3fb0a869bb5768ffaf20a89f78bb94ac98d46f
AnTaeWook <antk7894@naver.com>
2023-06-28 22:13:07 +0900

    init project

---
+++ src/main/java/com/antk7894/amatda/AmatdaApplication.java
@@ -0,0 +1,13 @@
+package com.antk7894.amatda;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AmatdaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AmatdaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/antk7894/amatda/AmatdaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.antk7894.amatda;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AmatdaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
