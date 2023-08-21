4d9b265e184f0fb0fbef13132dc42a7ce8bcf7c0
AnTaeWook <antk7894@naver.com>
2023-08-21 18:45:04 +0900

    feat: dto 정의
    daily CRUD를 구현하기 위한 dto 들을 정의하였음.

---
+++ src/main/java/com/antk7894/amatda/dto/daily/DailyPatchRequestDto.java
@@ -0,0 +1,6 @@
+package com.antk7894.amatda.dto.daily;
+
+public record DailyPatchRequestDto (
+        DailyAction action
+) {
+}
---
+++ src/main/java/com/antk7894/amatda/dto/daily/DailyUpdateRequestDto.java
@@ -0,0 +1,7 @@
+package com.antk7894.amatda.dto.daily;
+
+public record DailyUpdateRequestDto (
+        String title,
+        String description
+) {
+}
